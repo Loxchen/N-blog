79e02f174fc2d730b3f3f123b5fd9df3559711c1
loxchen <loxchen@hotmail.com>
2014-05-01 11:36:31 +0800

    Add date

--- models/post.js
+++ models/post.js
@@ -5,7 +5,8 @@ var Schema = mongoose.Schema;
 var Post = new Schema({
 	name: { type: String },
 	title: {type: String},
-	post: {type: String}
+	post: {type: String},
+	time: {type: Date, default: Date.now}
 });
 
 module.exports = mongoose.model('Post', Post);
